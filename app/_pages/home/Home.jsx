75530ff168dac27b5c3e4775de3faa532057a886
atik65 <atikhasan801@gmail.com>
2025-02-12 15:21:59 +0600

    fix

--- app/_pages/home/Home.jsx
+++ app/_pages/home/Home.jsx
@@ -11,15 +11,15 @@ import { HomeContent } from "./HomeContent";
 export function Home() {
   const links = [
     {
-      label: "Dashboard",
-      href: "#",
+      label: "Crimes",
+      href: "/",
       icon: (
         <LayoutDashboard className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
       ),
     },
     {
       label: "Profile",
-      href: "#",
+      href: "/profile",
       icon: (
         <UserCog className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
       ),
